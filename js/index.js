294a4fd100b27d9d7611ae4a065fdcb76d76433e
Alkesh Srivastava <alkeshsrivastava11@gmail.com>
2017-06-20 19:08:43 +0530

    Added icons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-var app = angular.module('app', ['ui.grid']);
+// // var app = angular.module('app', ['ui.grid']);
+//
+// // URL Routing
+//
+// var myApp = angular.module('myApp', ['ngRoute']);
+//
+// myApp.config(function($routeProvider) {
+//   $routeProvider
+//   .when('/',{
+//     templateUrl: 'main.html',
+//     controller: 'mainController'
+//   })
+//
+//   .when('/dashboard',{
+//     templateUrl: 'manage.html',
+//     controller: 'dashController'
+//   })
+//   // .when("/manage-registrations", {
+//   //   templateUrl: "",
+//   //   controller: 'manageController'
+//   // })
+//   // .when("/import", {
+//   //   templateUrl: "",
+//   //   controller: 'importController'
+//   // })
+//   // .when("/faq", {
+//   //   templateUrl: "",
+//   //   controller: 'faqController'
+//   // })
+//   // .when("/batch-assignment", {
+//   //   templateUrl: "",
+//   //   controller: 'batchController'
+//   // });
+// });
+//
+// console.log("routed");
+// myApp.controller('mainController', function($scope) {
+//   $scope.message = 'Alkesh';
+//   console.log("message 1 printed");
+// });
+//
+// console.log("routed for second time");
+// myApp.controller('dashController', function($scope) {
+//   $scope.message = 'Dashboard Page';
+//   console.log("message 2 printed");
+// });
